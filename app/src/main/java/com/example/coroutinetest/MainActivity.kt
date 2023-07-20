854e0751ab178be3023f0bb41c05f0572031b950
Blue <bluepark@yanadoocorp.com>
2023-07-20 17:37:34 +0900

    [main] add log

--- app/src/main/java/com/example/coroutinetest/MainActivity.kt
+++ app/src/main/java/com/example/coroutinetest/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.e("MainActivity", "main branch")
         setContent {
             CoroutineTestTheme {
                 // A surface container using the 'background' color from the theme
