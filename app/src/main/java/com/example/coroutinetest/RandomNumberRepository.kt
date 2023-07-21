98f109b2c1869e4fb00e94d7fdc3acbafafa7b7e
Blue <bluepark@yanadoocorp.com>
2023-07-21 09:47:17 +0900

    [init] git add

--- app/src/main/java/com/example/coroutinetest/RandomNumberRepository.kt
+++ app/src/main/java/com/example/coroutinetest/RandomNumberRepository.kt
@@ -5,6 +5,5 @@ import kotlinx.coroutines.flow.filter
 class RandomNumberRepository(
     private val randomNumberDataSource: RandomNumberDataSource
 ) {
-
     fun getRandomNumber() = randomNumberDataSource.getRandomNumber().filter { it < 9 }
 }
